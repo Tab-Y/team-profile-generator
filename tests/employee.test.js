a3159ce3bf5b077eae97cd3131201bb4ef1b5928
Tab-Y <tlyoumans@gmail.com>
2022-10-26 00:18:11 -0400

    start structure to tests

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,14 @@
 // this is where the tests for the employee class will go
+const Employee = require('../lib/Employee')
+
+describe("employee", () => {
+    describe("initialization", () => {
+        it("should", () => {
+            // hardcode variables to check function
+
+            expect(" ").toEqual(" ");
+        });
+
+        // make test for input and data collected
+    })
+})
